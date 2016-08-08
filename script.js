632611467da2929399a5fc2f5f2e666c80a03d9c
Sheng0o0 <shenghaozheng@mybihs.org>
2016-08-08 20:44:52 +0000

    Done

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
-//sets element to the variable $pic
-var pic = $("#pic");
-var ground = $('#ground');
-var picBottom = pic.top + pic.length;
-var groundTop = ground.top;
 $(document).ready(function() {
+    //sets element to the variable $pic
+    var pic = $("#pic");
+    var ground = $('#ground');
+    var picTop = $("#pic").offset().top;
+    var picBottom = picTop + $('#pic').height();
+    var groundTop = $("#ground").offset().top;
+    var groundBottom = groundTop + $('#ground').height();
+
+    function pixelate(numOfPixels) {
+        return numOfPixels + "px";
+    }
+
     $('#pic').click(function() {
 
 
         var myVar = setInterval(function() {
-            gravity()
-        }, 1000);
 
-        function gravity() {
-            console.log("1second");
-            $('#pic').top = $('#pic').top + 10;
-            $("#pic").css("top", $('#pic').top);
-        }
+            picBottom = picTop + $('#pic').height();
 
-        function myStopFunction() {
-            if (picBottom === groundTop) {
+            picTop = picTop + 1;
+            pic.css("top", pixelate(picTop));
+
+            if (picBottom >= groundTop) {
                 clearInterval(myVar);
             }
-        }
-
+        }, 8);
 
 
     });
