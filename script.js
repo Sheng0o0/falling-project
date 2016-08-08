b4700c3a61e38d74263d45679e38fdc775ab9661
Sheng0o0 <shenghaozheng@mybihs.org>
2016-08-08 17:21:37 +0000

    set basic code

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 //sets element to the variable $pic
-var $pic = $("#pic"); 
-
-$pic.click(function(){
+var pic = $("#pic"); 
+var ground=$('#ground');
+var picBottom = pic.top+pic.length;
+var groundTop = ground.top;
+pic.click(function(){setInterval(function(){interval()
    //Your code goes here
+   function interval(){
+        var num1= console.log("1second");
+        var num2 = pic.top = pic.top + 10;
+        var num3 = $("#pic").css("top", pic.top);
+   }
     });
+    
+    if(groundTop===picBottom){
+        clearInterval();
+    }
+    
+});
+       
+       
+   
